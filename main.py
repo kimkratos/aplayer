f6d14df7707c0d57b287aab6d9fa960146960259
kimkratos <tykratos@itcom888.com>
2024-07-03 11:05:37 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = '/root/music/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'File uploaded successfully'
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
