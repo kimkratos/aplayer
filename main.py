780d15683a3d000d6e4a58d652b6217f0e61886e
kimkratos <tykratos@itcom888.com>
2024-08-14 16:27:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,51 @@ import os
 import logging
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '/root/music/'
+UPLOAD_FOLDER = '/var/www/music/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Set up logging
 logging.basicConfig(filename='access.log', level=logging.INFO)
+@app.route('/ip')
+def ip_heatmap():
+    with open('map.html') as f:
+        return render_template_string(f.read())
+
+@app.route('/gen')
+def ip_genheatmap():
+    with open('heatmap.html') as f:
+        return render_template_string(f.read())
+
+@app.route('/generate_heatmap', methods=['POST'])
+def generate_heatmap():
+    # 读取 access.log 文件
+    with open('access.log', 'r') as file:
+        log_data = file.read()
+
+    # 查找所有 'Access from' 后面的 IP 地址
+    ips = re.findall(r'Access from ([\d\.]+)', log_data)
+
+    # 使用 GeoIP2 数据库进行 IP 地址到地理位置的转换
+    reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
+
+    locations = []
+    for ip in ips:
+        try:
+            response = reader.city(ip)
+            locations.append((response.location.latitude, response.location.longitude))
+        except geoip2.errors.AddressNotFoundError:
+            continue
+
+    # 统计每个地理位置的出现次数
+    location_counts = Counter(locations)
+
+    # 生成热力图
+    m = Map(location=[35.8617, 104.1954], zoom_start=4)
+    heat_data = [[loc[0], loc[1], count] for loc, count in location_counts.items()]
+    HeatMap(heat_data).add_to(m)
+    m.save('map.html')
+
+    return jsonify({"status": "success"})
 
 @app.route('/')
 def home():
